d464e9fe7e4f52082b252960b26433d086fcc036
sravyavulisetty <sravyavulisetty269@gmail.com>
2023-11-07 19:19:47 -0500

    committed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React, { useState, CSSProperties } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { MdOutlineLightMode } from "react-icons/md";
 import { MdDarkMode } from "react-icons/md";
 const App: React.FC = ()=>{
   const [theme, setTheme] = useState("light");
   const handleTheme =()=>{
-    setTheme((prev)=>(prev === "light" ? "dark" : "light"))
+    setTheme((prev)=>prev === "light" ? "dark" : "light")
   }
-  const rootStyle: React.CSSProperties = {
-    backgroundColor: theme === 'light' ? 'var(--bg-light)' : 'var(--bg-dark)',
-    color: theme === 'light' ? 'var(--color-light)' : 'var(--color-dark)',
-  };  
+  if(theme==="dark"){
+    document.documentElement.setAttribute('data-theme', "dark");
+    }
+  if(theme === "light"){
+    document.documentElement.setAttribute('data-theme', "light");
+    }
   return (
-    <div className="App" style={rootStyle}>
+    <div className="App">
       {theme === "dark" ?
       <div className='icon'>
         <MdOutlineLightMode size={25} onClick={handleTheme}/><span>Light</span>
@@ -20,9 +22,9 @@ const App: React.FC = ()=>{
       <div className='icon'>
         <MdDarkMode size={25} onClick={handleTheme}/><span>Dark</span>
       </div>}
-      <header className="App-header" style={rootStyle}>
+      <div className="App-header">
         <h1>Welcome to ScorePoint</h1>
-      </header>
+      </div>
     </div>
   );
 }
