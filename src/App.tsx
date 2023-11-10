f002ea8d72881a0c29e54b9f79aedb3592720f56
sravyavulisetty <sravyavulisetty269@gmail.com>
2023-11-10 11:41:56 -0500

    committed

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
-import React, { useState } from 'react';
+import {useState} from 'react';
 import './App.css';
 import { MdOutlineLightMode } from "react-icons/md";
 import { MdDarkMode } from "react-icons/md";
-const App: React.FC = ()=>{
-  const [theme, setTheme] = useState("light");
-  const handleTheme =()=>{
-    setTheme((prev)=>prev === "light" ? "dark" : "light")
+import {ThemeContext} from './Contexts/ThemeContext';
+const App:React.FC =() => {
+  const [theme, setTheme] = useState("dark");
+  const toggleTheme =()=>{
+    setTheme((prev)=>prev === "dark"?"light":"dark");
   }
-  if(theme==="dark"){
-    document.documentElement.setAttribute('data-theme', "dark");
-    }
-  if(theme === "light"){
-    document.documentElement.setAttribute('data-theme', "light");
-    }
   return (
-    <div className="App">
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+    <div className="App" data-theme={theme}>
       {theme === "dark" ?
-      <div className='icon'>
-        <MdOutlineLightMode size={25} onClick={handleTheme}/><span>Light</span>
+      <div className='icon' onClick={toggleTheme}>
+        <MdOutlineLightMode size={25}/><span>Light</span>
       </div> :
-      <div className='icon'>
-        <MdDarkMode size={25} onClick={handleTheme}/><span>Dark</span>
+      <div className='icon' onClick={toggleTheme}>
+        <MdDarkMode size={25}/><span>Dark</span>
       </div>}
       <div className="App-header">
         <h1>Welcome to ScorePoint</h1>
       </div>
     </div>
+    </ThemeContext.Provider>
   );
 }
 
---
+++ src/Contexts/ThemeContext.tsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+type themeContextType = {
+  theme: string,
+  toggleTheme:(e: any) => void;
+}
+export const ThemeContext = createContext<themeContextType>({theme: "", toggleTheme: () =>{}})
