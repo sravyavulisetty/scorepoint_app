078fc98b06f626021cf5265d1a95390b086124aa
sravyavulisetty <sravyavulisetty269@gmail.com>
2023-11-13 10:09:40 -0500

    committed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import './index.css';
 import { MdOutlineLightMode } from "react-icons/md";
 import { MdDarkMode } from "react-icons/md";
 import { ThemeContext } from './Contexts/ThemeContext';
@@ -10,16 +11,16 @@ const App: React.FC = ()=>{
   }
     return (
       <ThemeContext.Provider value={{theme, toggleTheme}}>
-    <div className="App" data-theme={theme}>
+    <div className="flex justify-center items-center relative text-center font-roboto data-[theme=dark]:text-textcolor data-[theme=dark]:bg-bgcolor data-[theme=light]:bg-bgcolor data-[theme=light]:text-textcolor" data-theme={theme}>
       {theme === "dark" ?
-      <div className='icon' onClick={toggleTheme}>
-        <MdOutlineLightMode size={25}/><span>Light</span>
+      <div className='absolute top-0 right-6 flex flex-row justify-center items-center' onClick={toggleTheme}>
+        <MdOutlineLightMode size={25}/><span className='p-2'>Light</span>
       </div> :
-      <div className='icon' onClick={toggleTheme}>
-        <MdDarkMode size={25}/><span>Dark</span>
+      <div className='absolute top-0 right-6 flex flex-row justify-center items-center' onClick={toggleTheme}>
+        <MdDarkMode size={25}/><span className='p-2'>Dark</span>
       </div>}
-      <div className="App-header">
-        <h1>Welcome to ScorePoint</h1>
+      <div className="flex flex-col min-h-screen justify-center items-center font-roboto">
+        <h1 className='text-3xl font-extrabold'>Welcome to ScorePoint</h1>
       </div>
     </div>
     </ThemeContext.Provider>
