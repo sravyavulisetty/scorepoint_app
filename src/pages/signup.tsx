1afce44689ce46caa7c8cae8c221a0e439278824
sravyavulisetty <sravyavulisetty269@gmail.com>
2023-11-30 13:34:29 -0500

    final loginpage commit

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import google from '../assets/icons8-google.svg';
 const Signup = () => {
   return (
-     <div className="flex items-center justify-center mt-1 m-4">
+     <div className="flex items-center justify-center h-screen">
      <div className="pt-1 rounded-lg shadow-gray-200 shadow-md w-auto flex flex-col justify-start">
        <h1 className="text-2xl font-bold text-center">CREATE AN ACCOUNT</h1>
        <form className='mx-16 mb-6 mt-5'>
@@ -10,27 +10,27 @@ const Signup = () => {
           <div className='flex flex-col'>
           <label className='block text-md my-1'>First Name</label>
           <div className="rounded-lg bg-gray-200 focus-within:bg-bggradient p-0.5 mb-3">
-            <input type='text' placeholder='First Name' className="p-1 rounded-md focus:outline-none text-black"></input>
+            <input type='text' className="p-1 rounded-md focus:outline-none text-black"></input>
           </div>
           </div>
           <div className='flex flex-col'>
           <label className='block text-md my-1'>Last Name</label>
           <div className="rounded-lg bg-gray-200 focus-within:bg-bggradient p-0.5 mb-3">
-            <input type='text' placeholder='Last name' className="p-1 rounded-md focus:outline-none text-black"></input>
+            <input type='text' className="p-1 rounded-md focus:outline-none text-black"></input>
           </div>
           </div>
         </div>
         <label className='block text-md my-1'>Email</label>
         <div className="rounded-lg bg-gray-200 focus-within:bg-bggradient p-0.5 mb-3">
-          <input type='email' placeholder='Email' className="w-full p-1 rounded-md focus:outline-none text-black"></input>
+          <input type='email' className="w-full p-1 rounded-md focus:outline-none text-black"></input>
         </div>
         <label className='block text-md my-1'>Password</label>
         <div className="rounded-lg bg-gray-200 focus-within:bg-bggradient p-0.5 mb-3">
-          <input type='password' placeholder='Password' className="w-full p-1 rounded-md focus:outline-none text-black"></input>
+          <input type='password' className="w-full p-1 rounded-md focus:outline-none text-black"></input>
         </div>
         <label className='block text-md my-1'>Confirm Password</label>
         <div className="rounded-lg bg-gray-200 focus-within:bg-bggradient p-0.5 mb-3">
-          <input type='password' placeholder='Confirm Password' className="w-full p-1 rounded-md focus:outline-none text-black"></input>
+          <input type='password' className="w-full p-1 rounded-md focus:outline-none text-black"></input>
         </div>
         <button className='bg-bggradient p-1 w-full text-center rounded mt-4 mb-4'>Register</button>
         <p>Already have an account, <Link to={'/login'}><button className="font-bold text-lg hover:text-indigo-500">Login</button></Link></p>
